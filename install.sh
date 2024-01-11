0831818a07df98af2168ec64694620b68c218977
Ugotapi <rudischmitz@gmail.com>
2024-01-10 22:09:49 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ wget http://www.galaxyfaraway.com/Sounds/SEEKYODA.WAV
 wget https://raw.githubusercontent.com/ugotapi/overheadpagingpi/main/play1.php
 sudo cp /home/$USER/index.html /var/www/html
 sudo cp /home/$USER/SEEKYODA.WAV /var/www/html/seekyoda.wav
-sudo cp /home/$USER/play1.php /vasr/www/html
+sudo cp /home/$USER/play1.php /var/www/html
 sudo chown www-data:www-data /var/www/html/play1.php
 sudo chown www-data:www-data /var/www/html/index.html
 sudo chown www-data:www-data /var/www/html/seekyoda.wav
