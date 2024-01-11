8eec655c9cc57ac0b29e6de5f76db6b765706f55
Ugotapi <rudischmitz@gmail.com>
2024-01-11 11:21:42 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,20 +4,19 @@ sudo rm -rf /var/www/html/index.html
 cd /home/$USER
 
 wget https://raw.githubusercontent.com/ugotapi/overheadpagingpi/main/index.html
-wget http://www.galaxyfaraway.com/Sounds/SEEKYODA.WAV
+wget https://www.thesoundarchive.com/starwars/alwaystwo.mp3
 wget https://www.thesoundarchive.com/starwars/swvader03.mp3
 wget https://raw.githubusercontent.com/ugotapi/overheadpagingpi/main/play1.php
 
 sudo cp /home/$USER/swvader03.mp3 /var/www/html
 sudo cp /home/$USER/index.html /var/www/html
-sudo cp /home/$USER/SEEKYODA.WAV /var/www/html/seekyoda.wav
+sudo cp /home/$USER/alwaystwo.mp3 /var/www/html
 sudo cp /home/$USER/play1.php /var/www/html
 
 sudo chown www-data:www-data /var/www/html/swvader03.mp3
 sudo chown www-data:www-data /var/www/html/play1.php
 sudo chown www-data:www-data /var/www/html/index.html
-sudo chown www-data:www-data /var/www/html/seekyoda.wav
-
+sudo chown www-data:www-data /var/www/html/alwaystwo.mp3
 # Create a backup of the original config.txt file
 sudo cp /boot/config.txt /boot/config.txt.backup
 
