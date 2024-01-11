fdd24a25aefade379ca6df4caa26aa23d7adbe1b
Ugotapi <rudischmitz@gmail.com>
2024-01-11 08:10:10 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,5 +12,14 @@ sudo chown www-data:www-data /var/www/html/play1.php
 sudo chown www-data:www-data /var/www/html/index.html
 sudo chown www-data:www-data /var/www/html/seekyoda.wav
 
+# Create a backup of the original config.txt file
+sudo cp /boot/config.txt /boot/config.txt.backup
+
+# Use sed to replace the line in the config.txt file
+sudo sed -i 's/dtoverlay=vc4-kms-v3d/dtoverlay=vc4-kms-v3d,noaudio/' /boot/config.txt
+
+read -p "After this reboot go to your tablet/touch pc and put in the ip address of this device.  Hit Enter key to continue"
+sudo reboot
+
 
 
