d020c2c5bcfbc597ba837725be9583d4139d9a98
Ugotapi <rudischmitz@gmail.com>
2024-01-10 21:44:59 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,5 +2,6 @@ sudo apt install apache2 php sox libsox-fmt-mp3
 sudo usermod -aG audio www-data
 sudo rm -rf /var/www/html/index.html
 cd /var/www/html
-wget 
-wget --help
+wget https://raw.githubusercontent.com/ugotapi/overheadpagingpi/main/index.html
+chown www-data:www-data /var/www/html/index.html
+
