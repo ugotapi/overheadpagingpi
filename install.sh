026f23a06af677c3a1d4e781b621908b3b6f57cd
Ugotapi <rudischmitz@gmail.com>
2024-01-10 21:47:31 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,5 +3,8 @@ sudo usermod -aG audio www-data
 sudo rm -rf /var/www/html/index.html
 cd /var/www/html
 wget https://raw.githubusercontent.com/ugotapi/overheadpagingpi/main/index.html
+wget http://www.galaxyfaraway.com/Sounds/yoda-help_you_i_can.mp3
 chown www-data:www-data /var/www/html/index.html
+chown www-data:www-data /var/www/html/yoda-help_you_i_can.mp3
+
 
