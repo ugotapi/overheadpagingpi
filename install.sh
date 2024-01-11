131148fccd2aac0b603685edded498b6b052882a
Ugotapi <rudischmitz@gmail.com>
2024-01-11 08:49:39 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,17 +2,15 @@ sudo apt install apache2 php sox libsox-fmt-mp3
 sudo usermod -aG audio www-data
 sudo rm -rf /var/www/html/index.html
 cd /home/$USER
-wget https://github.com/ugotapi/overheadpagingpi/blob/main/yourlogo.png
+
 wget https://raw.githubusercontent.com/ugotapi/overheadpagingpi/main/index.html
 wget http://www.galaxyfaraway.com/Sounds/SEEKYODA.WAV
 wget https://raw.githubusercontent.com/ugotapi/overheadpagingpi/main/play1.php
 
-sudo cp /home/$USER/yourlogo.png /var/www/html
 sudo cp /home/$USER/index.html /var/www/html
 sudo cp /home/$USER/SEEKYODA.WAV /var/www/html/seekyoda.wav
 sudo cp /home/$USER/play1.php /var/www/html
 
-sudo chown www-data:www-data /var/ww/html/yourlogo.png
 sudo chown www-data:www-data /var/www/html/play1.php
 sudo chown www-data:www-data /var/www/html/index.html
 sudo chown www-data:www-data /var/www/html/seekyoda.wav
