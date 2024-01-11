b8051392e6ec01a184835397247d5c7719049ec4
Ugotapi <rudischmitz@gmail.com>
2024-01-11 11:40:20 -0600

    Update play1.php

--- play1.php
+++ play1.php
@@ -1,5 +1,5 @@
 TRUCKRECEIVING.php
 <?php
-exec('/usr/bin/play /var/www/html/alwaystwo.mp3');
+exec('/usr/bin/mplayer -vo null /var/www/html/alwaystwo.mp3');
 header('Location: ../index.html?success=true');
 ?>
