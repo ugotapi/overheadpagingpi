8a94dfc4cd8f1f9c573cc973b81968d410b39849
Ugotapi <rudischmitz@gmail.com>
2024-01-11 13:41:32 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -17,11 +17,6 @@ sudo chown www-data:www-data /var/www/html/swvader03.mp3
 sudo chown www-data:www-data /var/www/html/play1.php
 sudo chown www-data:www-data /var/www/html/index.html
 sudo chown www-data:www-data /var/www/html/alwaystwo.mp3
-# Create a backup of the original config.txt file
-sudo cp /boot/config.txt /boot/config.txt.backup
-
-# Use sed to replace the line in the config.txt file this disables HDMI audio device. If you have a USB sound device attached, it then becomes the default. 
-sudo sed -i 's/dtoverlay=vc4-kms-v3d/dtoverlay=vc4-kms-v3d,noaudio/' /boot/config.txt
 
 read -p "After this reboot go to your tablet/touch pc and put in the ip address of this device.  Hit Enter key to continue"
 sudo reboot
