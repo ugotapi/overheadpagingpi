29d415b1975ff750285eab37e4f91130542d5ba4
Ugotapi <rudischmitz@gmail.com>
2024-01-14 13:44:46 -0600

    Update play1.php

--- play1.php
+++ play1.php
@@ -1,4 +1,4 @@
-//  for using the HDMI soundcard you can put -ao alsa
+//  for using the HDMI soundcard you can put -ao pulse
 <?php
 exec('/usr/bin/mplayer -vo null -ao alsa:device=hw=2,0 /var/www/html/alwaystwo.mp3');
 header('Location: ../index.html?success=true');
