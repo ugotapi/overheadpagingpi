6c4cb8332a82f9a22cc68b6c38c82f9e703cf9d0
Ugotapi <rudischmitz@gmail.com>
2024-05-01 19:23:25 -0500

    Update play1.php

--- play1.php
+++ play1.php
@@ -1,4 +1,4 @@
-//  for using the HDMI sound you can put "-ao pulse"
+//  for using the HDMI sound you can put "-ao pulse" if you need to
 <?php
 exec('/usr/bin/mplayer -vo null -ao alsa:device=hw=2,0 /var/www/html/alwaystwo.mp3');
 header('Location: ../index.html?success=true');
